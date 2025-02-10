4c31ffd2317084abc1f19baa76a43d990ad91ba5
Chewbacca <higgimat000@warren.k12.in.us>
2025-02-10 13:41:32 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> arrayList = new ArrayList<>();
-        arrayList.add("Abel");
         arrayList.add("Benjamin");
-        arrayList.remove("Cain");
-        arrayList.add("David");
+        arrayList.add("Abel");
+        arrayList.remove("David");
         arrayList.add("Ezekiel");
-
+        arrayList.add("Cain");
+        Collections.sort(arrayList);
         System.out.println("ArrayList: " + arrayList);
     }
 }
